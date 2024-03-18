42d266e546c435dce92796d84c20432b3fad71f9
Carlos PC <xdeus.v2@gmail.com>
2024-03-18 08:41:06 -0600

    Static and Public properties

--- POO/src/Classes/Transaction.php
+++ POO/src/Classes/Transaction.php
@@ -2,12 +2,26 @@
 
 class Transaction
 {
+    
     public const STATUS_PAID = 'paid';
     public const STATUS_PENDING = 'pending';
     public const STATUS_DECLINED = 'delcined';
 
-    public function __construct($status){
+    static public int $count = 0;
+
+    public float $amount;
+    public string $description;
+
+    public function __construct($amount,$description,$status){
+       $this->amount = $amount;
+       $this->description = $description;
        $this->status = $status;
-       
+       self::$count++;
+    }
+
+    public function printTransaction() {
+        echo 'amount: ' . $this->amount . '<br>';
+        echo 'description: ' . $this->description . '<br>';
+        echo 'status: ' . $this->status . '<br>';
     }
 }
--- POO/src/public/index.php
+++ POO/src/public/index.php
@@ -4,6 +4,10 @@ require('../vendor/autoload.php');
 
 use App\Classes\Transaction;
 
-$transaction = new Transaction('not paid');
-$status = $transaction->status;
-echo $status . '<br>';
+$transaction = new Transaction(100,'burger place','not paid');
+
+$transaction->printTransaction(); // ACCESSS FUNCTION
+var_dump($transaction);
+echo '<br>';
+var_dump($transaction::$count); // ACCESS STATIC PROPERTIES
+
