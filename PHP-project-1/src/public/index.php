174172011b1b28dc4f3b68c497978d57b09eb4f0
valium69mg <xdeus.v2@gmail.com>
2024-04-15 09:09:47 -0600

    Added more code to the index page \n 1. Added a PDO statement simple example with placeholders \n 2. Added a PDO statement with named paramenerts

--- PHP-project-1/src/public/index.php
+++ PHP-project-1/src/public/index.php
@@ -4,7 +4,7 @@ $table = 'Persons';
 $db = new PDO('mysql:host=db;dbname=php1','root','root',
     [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]
 );
-
+// PREPARED STATEMENTS 
 try {
     $query = 'SELECT * FROM Persons';
     $stmt = $db->query($query);
@@ -18,5 +18,37 @@ try {
     throw new \PDOException($e->getMessage(), $e->getCode());
 }
 
+// PDO STATEMENTS WITH NAMED PARAMETERS 
+try {
+    $name = 'Carlos';
+    $query = 'SELECT * FROM Persons WHERE FirstName = ?';
+    $stmt = $db->prepare($query);
+    $stmt->execute([$name]);
+
+
+    foreach($stmt->fetchAll() as $user) {
+        echo '<pre>';
+        var_dump($user);
+        echo '<pre>';
+    }
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), $e->getCode());
+}
+// PDO WITH NAMED 
+try {
+    $name = 'Vicky';
+    $query = 'SELECT * FROM Persons WHERE FirstName = :name';
+    $stmt = $db->prepare($query);
+    $stmt->execute(['name' => $name]);
+
+
+    foreach($stmt->fetchAll() as $user) {
+        echo '<pre>';
+        var_dump($user);
+        echo '<pre>';
+    }
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), $e->getCode());
+}
 
 
