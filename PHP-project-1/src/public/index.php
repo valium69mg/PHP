b0d8b9250050248583b92379c57e2e5ecc3c2fe2
valium69mg <xdeus.v2@gmail.com>
2024-04-15 08:54:06 -0600

    Added mysql and made the connection work, also made a simple basic query to extract all info from a table

--- PHP-project-1/src/public/index.php
+++ PHP-project-1/src/public/index.php
@@ -1,3 +1,22 @@
 <?php
+$database = 'php1';
+$table = 'Persons';
+$db = new PDO('mysql:host=db;dbname=php1','root','root',
+    [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]
+);
 
-echo 'Hello from docker!';
+try {
+    $query = 'SELECT * FROM Persons';
+    $stmt = $db->query($query);
+
+    foreach($stmt->fetchAll() as $user) {
+        echo '<pre>';
+        var_dump($user);
+        echo '<pre>';
+    }
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), $e->getCode());
+}
+
+
+
